f60fd8fc881e155c48bed84cfed6cface8ac00d5
Mallika <mallikak2709@gmail.com>
2015-10-29 15:50:45 -0700

    First two pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/bigdata', function(req, res, next) {
+    res.render('bigdata');
+});
+
+router.get('/brochure', function(req, res, next) {
+    res.render('brochure');
+});
+
+router.get('/whitepaper', function(req, res, next) {
+    res.render('whitepaper');
+});
+
+router.get('/iot', function(req, res, next) {
+    res.render('iot');
 });
 
 module.exports = router;
